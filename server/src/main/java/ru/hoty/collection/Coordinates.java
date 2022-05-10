8f507dbb6143f3918db576adb645931885784f08
hoty <iiec1337@yandex.ru>
2022-05-10 20:38:53 +0300

    a little changes of code structure

--- server/src/main/java/ru/hoty/collection/Coordinates.java
+++ server/src/main/java/ru/hoty/collection/Coordinates.java
@@ -3,8 +3,8 @@ package ru.hoty.collection;
 import java.io.Serializable;
 
 public class Coordinates implements Serializable {
-    private Integer x; //Максимальное значение поля: 846, Поле не может быть null
-    private Float y; //Максимальное значение поля: 672, Поле не может быть null
+    private final Integer x; //Максимальное значение поля: 846, Поле не может быть null
+    private final Float y; //Максимальное значение поля: 672, Поле не может быть null
 
     public Coordinates(Integer x, Float y) {
         this.x = x;
@@ -22,5 +22,14 @@ public class Coordinates implements Serializable {
 
     public Float getY() {
         return y;
+    }
+
+    public int compareTo(Coordinates other) {
+        if(getX() * getX() + getY() * getY() ==
+                other.getX() * other.getX() + other.getY() * other.getY()) {
+            return 0;
+        }
+        return (getX() * getX() + getY() * getY() >
+                other.getX() * other.getX() + other.getY() * other.getY() ? 1 : -1);
     }
 }
